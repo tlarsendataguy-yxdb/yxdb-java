d6c6fedb2a4e5c9baf36df6f7837ab903c4298d7
Tom Larsen <larsenthomasj@gmail.com>
2022-07-29 17:04:18 -0400

    read metainfo

--- src/main/java/com/tlarsendataguy/yxdb/Reader.java
+++ src/main/java/com/tlarsendataguy/yxdb/Reader.java
@@ -5,19 +5,28 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 public class Reader {
-    private Reader(long numRecords) {
+    private Reader(long numRecords, int metaInfoSize, String metaInfoStr) {
         this.numRecords = numRecords;
+        this.metaInfoSize = metaInfoSize;
+        this.metaInfoStr = metaInfoStr;
     }
     public long numRecords;
+    public int metaInfoSize;
+    public String metaInfoStr;
 
     public static Reader loadYxdb(String path) throws IllegalArgumentException, IOException {
         var file = new File(path);
         try (var stream = new FileInputStream(file)) {
             var header = getHeader(stream);
             var numRecords = header.getLong(104);
-            return new Reader(numRecords);
+            var metaInfoSize = header.getInt(80);
+            var metaInfoBytes = stream.readNBytes((metaInfoSize*2)-2); //YXDB strings are null-terminated, so exclude the last character
+            var metaInfoStr = new String(metaInfoBytes, StandardCharsets.UTF_16LE);
+            return new Reader(numRecords, metaInfoSize, metaInfoStr);
         }
     }
 
--- src/test/java/com/tlarsendataguy/yxdb/ReaderTest.java
+++ src/test/java/com/tlarsendataguy/yxdb/ReaderTest.java
@@ -3,8 +3,6 @@ package com.tlarsendataguy.yxdb;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
-import java.io.FileNotFoundException;
-
 public class ReaderTest {
     @Test
     public void TestGetReader() {
@@ -12,8 +10,32 @@ public class ReaderTest {
         try{
             var yxdb = Reader.loadYxdb(path);
             Assertions.assertEquals(1, yxdb.numRecords);
+            Assertions.assertEquals(1372, yxdb.metaInfoSize);
+            Assertions.assertNotNull(yxdb.metaInfoStr);
+            Assertions.assertEquals(AllNormalFieldsMetaXml, yxdb.metaInfoStr);
         } catch (Exception ex){
             Assertions.fail(ex.toString());
         }
     }
+
+    String AllNormalFieldsMetaXml = """
+<RecordInfo>
+	<Field name="ByteField" source="TextInput:" type="Byte"/>
+	<Field name="BoolField" source="Formula: 1" type="Bool"/>
+	<Field name="Int16Field" source="Formula: 16" type="Int16"/>
+	<Field name="Int32Field" source="Formula: 32" type="Int32"/>
+	<Field name="Int64Field" source="Formula: 64" type="Int64"/>
+	<Field name="FixedDecimalField" scale="6" size="19" source="Formula: 123.45" type="FixedDecimal"/>
+	<Field name="FloatField" source="Formula: 678.9" type="Float"/>
+	<Field name="DoubleField" source="Formula: 0.12345" type="Double"/>
+	<Field name="StringField" size="64" source="Formula: &quot;A&quot;" type="String"/>
+	<Field name="WStringField" size="64" source="Formula: &quot;AB&quot;" type="WString"/>
+	<Field name="V_StringShortField" size="1000" source="Formula: &quot;ABC&quot;" type="V_String"/>
+	<Field name="V_StringLongField" size="2147483647" source="Formula: PadLeft(&quot;&quot;, 500, &apos;B&apos;)" type="V_String"/>
+	<Field name="V_WStringShortField" size="10" source="Formula: &quot;XZY&quot;" type="V_WString"/>
+	<Field name="V_WStringLongField" size="1073741823" source="Formula: PadLeft(&quot;&quot;, 500, &apos;W&apos;)" type="V_WString"/>
+	<Field name="DateField" source="Formula: &apos;2020-01-01&apos;" type="Date"/>
+	<Field name="DateTimeField" source="Formula: &apos;2020-02-03 04:05:06&apos;" type="DateTime"/>
+</RecordInfo>
+""";
 }

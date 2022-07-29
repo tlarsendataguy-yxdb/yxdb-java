467c26f79fdd53122120525cc6de29fdbbc73642
Tom Larsen <larsenthomasj@gmail.com>
2022-07-29 08:52:28 -0400

    eliminate warning message

--- src/main/java/com/tlarsendataguy/yxdb/Reader.java
+++ src/main/java/com/tlarsendataguy/yxdb/Reader.java
@@ -2,7 +2,6 @@ package com.tlarsendataguy.yxdb;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.ByteOrder;
 import java.util.Arrays;
@@ -13,16 +12,16 @@ public class Reader {
     }
     public int numRecords;
 
-    public static Reader loadYxdb(String path) throws FileNotFoundException, IOException {
+    public static Reader loadYxdb(String path) throws IllegalArgumentException, IOException {
         var file = new File(path);
-        var stream = new FileInputStream(file);
-
-        var header = new byte[512];
-        var written = stream.read(header);
-        if (written < 512) {
-            throw new IllegalArgumentException(String.format("'%s' is an invalid yxdb file", path));
+        try (var stream = new FileInputStream(file)) {
+            var header = new byte[512];
+            var written = stream.read(header);
+            if (written < 512) {
+                throw new IllegalArgumentException(String.format("'%s' is an invalid yxdb file", path));
+            }
+            var numRecords = java.nio.ByteBuffer.wrap(Arrays.copyOfRange(header, 104,112)).order(ByteOrder.LITTLE_ENDIAN).getInt();
+            return new Reader(numRecords);
         }
-        var numRecords = java.nio.ByteBuffer.wrap(Arrays.copyOfRange(header, 104,112)).order(ByteOrder.LITTLE_ENDIAN).getInt();
-        return new Reader(numRecords);
     }
 }

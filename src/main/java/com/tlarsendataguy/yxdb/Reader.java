bd3bea90947b30bec96219ce6d83ca8c618c765c
Tom Larsen <larsenthomasj@gmail.com>
2022-07-29 09:06:50 -0400

    extract methods

--- src/main/java/com/tlarsendataguy/yxdb/Reader.java
+++ src/main/java/com/tlarsendataguy/yxdb/Reader.java
@@ -7,21 +7,30 @@ import java.nio.ByteOrder;
 import java.util.Arrays;
 
 public class Reader {
-    private Reader(int numRecords) {
+    private Reader(long numRecords) {
         this.numRecords = numRecords;
     }
-    public int numRecords;
+    public long numRecords;
 
     public static Reader loadYxdb(String path) throws IllegalArgumentException, IOException {
         var file = new File(path);
         try (var stream = new FileInputStream(file)) {
-            var header = new byte[512];
-            var written = stream.read(header);
-            if (written < 512) {
-                throw new IllegalArgumentException(String.format("'%s' is an invalid yxdb file", path));
-            }
-            var numRecords = java.nio.ByteBuffer.wrap(Arrays.copyOfRange(header, 104,112)).order(ByteOrder.LITTLE_ENDIAN).getInt();
+            var header = getHeader(stream);
+            var numRecords = extractLong(header, 104);
             return new Reader(numRecords);
         }
+    }
+
+    private static byte[] getHeader(FileInputStream stream) throws IOException {
+        var header = new byte[512];
+        var written = stream.readNBytes(header,0, 512);
+        if (written < 512) {
+            throw new IllegalArgumentException("file is an invalid yxdb file");
+        }
+        return header;
+    }
+
+    private static long extractLong(byte[] data, int startAt) {
+        return java.nio.ByteBuffer.wrap(Arrays.copyOfRange(data, startAt,startAt+8)).order(ByteOrder.LITTLE_ENDIAN).getLong();
     }
 }

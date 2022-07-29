fa92bd3cae64fa1b13aeecf5aa377197615fcfda
Tom Larsen <larsenthomasj@gmail.com>
2022-07-29 15:13:58 -0400

    wrap header in ByteBuffer to prevent allocations when reading values

--- src/main/java/com/tlarsendataguy/yxdb/Reader.java
+++ src/main/java/com/tlarsendataguy/yxdb/Reader.java
@@ -3,8 +3,8 @@ package com.tlarsendataguy.yxdb;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
-import java.util.Arrays;
 
 public class Reader {
     private Reader(long numRecords) {
@@ -16,21 +16,17 @@ public class Reader {
         var file = new File(path);
         try (var stream = new FileInputStream(file)) {
             var header = getHeader(stream);
-            var numRecords = extractLong(header, 104);
+            var numRecords = header.getLong(104);
             return new Reader(numRecords);
         }
     }
 
-    private static byte[] getHeader(FileInputStream stream) throws IOException {
-        var header = new byte[512];
-        var written = stream.readNBytes(header,0, 512);
+    private static ByteBuffer getHeader(FileInputStream stream) throws IOException {
+        var headerBytes = new byte[512];
+        var written = stream.readNBytes(headerBytes,0, 512);
         if (written < 512) {
             throw new IllegalArgumentException("file is an invalid yxdb file");
         }
-        return header;
-    }
-
-    private static long extractLong(byte[] data, int startAt) {
-        return java.nio.ByteBuffer.wrap(Arrays.copyOfRange(data, startAt,startAt+8)).order(ByteOrder.LITTLE_ENDIAN).getLong();
+        return ByteBuffer.wrap(headerBytes).order(ByteOrder.LITTLE_ENDIAN);
     }
 }

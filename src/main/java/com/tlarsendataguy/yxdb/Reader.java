a268f5142edff5e161ee25811bc613a36cc2d2b4
Tom Larsen <larsenthomasj@gmail.com>
2022-07-30 07:18:20 -0400

    split loadMetaInfo into smaller methods

--- src/main/java/com/tlarsendataguy/yxdb/Reader.java
+++ src/main/java/com/tlarsendataguy/yxdb/Reader.java
@@ -2,6 +2,7 @@ package com.tlarsendataguy.yxdb;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -40,6 +41,10 @@ public class Reader {
         var header = getHeader();
         numRecords = header.getLong(104);
         metaInfoSize = header.getInt(80);
+        loadMetaInfo();
+    }
+
+    private void loadMetaInfo() throws IOException, IllegalArgumentException {
         var metaInfoBytes = stream.readNBytes((metaInfoSize*2)-2); //YXDB strings are null-terminated, so exclude the last character
         if (metaInfoBytes.length < (metaInfoSize*2)-2) {
             closeStreamAndThrow();
@@ -51,6 +56,7 @@ public class Reader {
         metaInfoStr = new String(metaInfoBytes, StandardCharsets.UTF_16LE);
         getFields();
     }
+
     private ByteBuffer getHeader() throws IOException, IllegalArgumentException {
         var headerBytes = new byte[512];
         var written = stream.readNBytes(headerBytes,0, 512);
@@ -61,18 +67,7 @@ public class Reader {
     }
 
     private void getFields() throws IllegalArgumentException {
-        Document doc;
-        try {
-            DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-            doc = builder.parse(new InputSource(new StringReader(metaInfoStr)));
-            doc.getDocumentElement().normalize();
-        } catch (Exception ex) {
-            closeStreamAndThrow();
-            return;
-        }
-
-        var info = doc.getElementsByTagName("RecordInfo").item(0);
-        var nodes = info.getChildNodes();
+        var nodes = getRecordInfoNodes();
         var index = 0;
 
         for (int i = 0; i < nodes.getLength(); i++) {
@@ -80,53 +75,72 @@ public class Reader {
             if (field.getNodeType() != Node.ELEMENT_NODE) {
                 continue;
             }
-            var attributes = field.getAttributes();
-            var name = attributes.getNamedItem("name");
-            var size = attributes.getNamedItem("size");
-            var type = attributes.getNamedItem("type");
-            var scale = attributes.getNamedItem("scale");
-
-            if (name == null || type == null) {
-                closeStreamAndThrow();
-                return;
-            }
-
-            var nameStr = name.getNodeValue();
-            switch (type.getNodeValue()) {
-                case "Byte" -> fields.add(new Field(index, nameStr, "Byte", 1, 0));
-                case "Bool" -> fields.add(new Field(index, nameStr, "Bool", 1, 0));
-                case "Int16" -> fields.add(new Field(index, nameStr, "Int16", 2, 0));
-                case "Int32" -> fields.add(new Field(index, nameStr, "Int32", 4, 0));
-                case "Int64" -> fields.add(new Field(index, nameStr, "Int64", 8, 0));
-                case "FixedDecimal" -> {
-                    if (scale == null || size == null) {
-                        closeStreamAndThrow();
-                        return;
-                    }
-                    fields.add(new Field(index, nameStr, "FixedDecimal", parseInt(size.getNodeValue()), parseInt(scale.getNodeValue())));
-                }
-                case "Float" -> fields.add(new Field(index, nameStr, "Float", 4, 0));
-                case "Double" -> fields.add(new Field(index, nameStr, "Double", 8, 0));
-                case "String" -> {
-                    if (size == null) {
-                        closeStreamAndThrow();
-                        return;
-                    }
-                    fields.add(new Field(index, nameStr, "String", parseInt(size.getNodeValue()), 0));
-                }
-                case "WString" -> {
-                    if (size == null) {
-                        closeStreamAndThrow();
-                        return;
-                    }
-                    fields.add(new Field(index, nameStr, "WString", parseInt(size.getNodeValue()) * 2, 0));
-                }
-                case "V_String", "V_WString", "Blob", "SpatialObj" -> fields.add(new Field(index, nameStr, type.getNodeValue(), 4, 0));
-                case "Date" -> fields.add(new Field(index, nameStr, "Date", 10, 0));
-                case "DateTime" -> fields.add(new Field(index, nameStr, "DateTime", 19, 0));
-                default -> closeStreamAndThrow();
-            }
+            parseField(field, index);
             index++;
+        }
+    }
+
+    private NodeList getRecordInfoNodes() {
+        Document doc;
+        try {
+            DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            doc = builder.parse(new InputSource(new StringReader(metaInfoStr)));
+            doc.getDocumentElement().normalize();
+        } catch (Exception ex) {
+            closeStreamAndThrow();
+            throw new IllegalArgumentException();
+        }
+
+        var info = doc.getElementsByTagName("RecordInfo").item(0);
+        return info.getChildNodes();
+    }
+
+    private void parseField(Node field, int index) {
+        var attributes = field.getAttributes();
+        var name = attributes.getNamedItem("name");
+        var size = attributes.getNamedItem("size");
+        var type = attributes.getNamedItem("type");
+        var scale = attributes.getNamedItem("scale");
+
+        if (name == null || type == null) {
+            closeStreamAndThrow();
+            return;
+        }
+
+        var nameStr = name.getNodeValue();
+        switch (type.getNodeValue()) {
+            case "Byte" -> fields.add(new Field(index, nameStr, "Byte", 1, 0));
+            case "Bool" -> fields.add(new Field(index, nameStr, "Bool", 1, 0));
+            case "Int16" -> fields.add(new Field(index, nameStr, "Int16", 2, 0));
+            case "Int32" -> fields.add(new Field(index, nameStr, "Int32", 4, 0));
+            case "Int64" -> fields.add(new Field(index, nameStr, "Int64", 8, 0));
+            case "FixedDecimal" -> {
+                if (scale == null || size == null) {
+                    closeStreamAndThrow();
+                    return;
+                }
+                fields.add(new Field(index, nameStr, "FixedDecimal", parseInt(size.getNodeValue()), parseInt(scale.getNodeValue())));
+            }
+            case "Float" -> fields.add(new Field(index, nameStr, "Float", 4, 0));
+            case "Double" -> fields.add(new Field(index, nameStr, "Double", 8, 0));
+            case "String" -> {
+                if (size == null) {
+                    closeStreamAndThrow();
+                    return;
+                }
+                fields.add(new Field(index, nameStr, "String", parseInt(size.getNodeValue()), 0));
+            }
+            case "WString" -> {
+                if (size == null) {
+                    closeStreamAndThrow();
+                    return;
+                }
+                fields.add(new Field(index, nameStr, "WString", parseInt(size.getNodeValue()) * 2, 0));
+            }
+            case "V_String", "V_WString", "Blob", "SpatialObj" -> fields.add(new Field(index, nameStr, type.getNodeValue(), 4, 0));
+            case "Date" -> fields.add(new Field(index, nameStr, "Date", 10, 0));
+            case "DateTime" -> fields.add(new Field(index, nameStr, "DateTime", 19, 0));
+            default -> closeStreamAndThrow();
         }
     }
 

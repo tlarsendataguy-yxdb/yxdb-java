81a10c6429439645325bd02202cdd8256ee06ce4
Tom Larsen <larsenthomasj@gmail.com>
2022-08-01 15:43:26 -0400

    refactor tests a bit

--- src/test/java/com/tlarsendataguy/yxdb/YxdbRecordTest.java
+++ src/test/java/com/tlarsendataguy/yxdb/YxdbRecordTest.java
@@ -7,35 +7,28 @@ import java.util.ArrayList;
 
 public class YxdbRecordTest {
     @Test
-    public void TestReadInt32Record() {
-        var fields = new ArrayList<MetaInfoField>(1);
-        fields.add(new MetaInfoField("value", "Int32", 4, 0));
-        var record = YxdbRecord.newFromFieldList(fields);
+    public void TestReadInt16Record() {
+        var record = loadRecordWithValueColumn("Int16", 2, new byte[]{23,0,0});
 
         Assertions.assertEquals(1, record.fields.size());
         Assertions.assertSame(Long.TYPE, record.fields.get(0).type());
         Assertions.assertEquals("value", record.fields.get(0).name());
+        Assertions.assertEquals(23, record.extractLongFrom(0));
+        Assertions.assertEquals(23, record.extractLongFrom("value"));
+    }
 
-        var sourceData = new byte[]{23,0,0,0,0};
-        record.loadRecordBlobFrom(sourceData, 0, 5);
+    @Test
+    public void TestReadInt32Record() {
+        var record = loadRecordWithValueColumn("Int32",4, new byte[]{23,0,0,0,0});
 
         Assertions.assertEquals(23, record.extractLongFrom(0));
     }
 
-    @Test
-    public void TestReadInt16Record() {
+    private static YxdbRecord loadRecordWithValueColumn(String type, int size, byte[] sourceData) {
         var fields = new ArrayList<MetaInfoField>(1);
-        fields.add(new MetaInfoField("value", "Int16", 2, 0));
+        fields.add(new MetaInfoField("value", type, size, 0));
         var record = YxdbRecord.newFromFieldList(fields);
-
-        Assertions.assertEquals(1, record.fields.size());
-        Assertions.assertSame(Long.TYPE, record.fields.get(0).type());
-        Assertions.assertEquals("value", record.fields.get(0).name());
-
-        var sourceData = new byte[]{23,0,0};
-        record.loadRecordBlobFrom(sourceData, 0, 3);
-
-        Assertions.assertEquals(23, record.extractLongFrom(0));
-        Assertions.assertEquals(23, record.extractLongFrom("value"));
+        record.loadRecordBlobFrom(sourceData, 0, sourceData.length);
+        return record;
     }
 }
